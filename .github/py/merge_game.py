f000f2044a6e58fdd1a5be51209fffa640086d4a
VoGter0616 <oldman0616@gmail.com>
2026-07-27 02:32:51 +0800

    Update merge_game.py

--- .github/py/merge_game.py
+++ .github/py/merge_game.py
@@ -22,7 +22,7 @@ def get_beijing_time():
 
 
 def merge_game_rules():
-    output_dir = "rule/Clash"
+    output_dir = "rule/Merged"
     output_path = os.path.join(output_dir, "Game_Merged.list")
 
     # 1. 读取旧文件用于比对新增数量
